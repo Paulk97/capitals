de565c472e595774a82111a380d70394af885142
Paulk97 <paolo.schiavulli@gmail.com>
2019-11-20 09:30:01 -0500

    sys

--- myfolder/main.py
+++ myfolder/main.py
@@ -1,14 +1,13 @@
 #! /usr/bin/env python3
 
+import sys
+
 '''
 From module Capitals we import both the key and the value in order to make available the function.
 '''
 
-
 from capitals import check_capital, check_state
 
-check_capital("Germany")
-check_capital("Honduras")
-check_state("Rome")
-check_state("Tokyo")
+check_capital(sys.argv[1])
+check_state(sys.argv[2])
 
