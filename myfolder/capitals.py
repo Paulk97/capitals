9bcc8c59e2fe08152cf605158f07deba76958256
Nicolò Rosato <nicolorosato@NicoloRosato.local>
2019-12-12 09:58:30 +0100

    capitals.py

--- myfolder/capitals.py
+++ myfolder/capitals.py
@@ -1,20 +1,19 @@
 import csv
 
 '''Reading csv file as dictionary
-
 '''
-csv_path= 'data/capitals.csv'
 
-reader = csv.reader(open(csv_path, 'r'))
-d = {}
-for row in reader:
-    k, v = row
-    d[k] = v
+def csv_reader(path):
+    reader = csv.reader(open(path, 'r'))
+    d = {}
+    for row in reader:
+        k, v = row
+        d[k] = v
 
 '''From the dictionary return the value (capital) of the input key(state)
-
 '''
 
+csv_reader('/Users/nicolorosato/capitals/data/capitals.csv')
 
 def check_capital(state):
     if state in d.keys():
